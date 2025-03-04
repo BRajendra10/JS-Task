fef6b63b3085116a1a8f11c9dae23c9e3f8b0f72
Rajendra Behera <rajendrabehera8116@gmail.com>
2025-03-04 08:15:52 +0530

    Update scirpt.js

--- Array/accending and decending/scirpt.js
+++ Array/accending and decending/scirpt.js
@@ -1,2 +1,31 @@
 // wap to arrange array in accending and decending
 
+let arr = [79, 65, 4, 99, 33, 10, 5, 125];
+
+// ascending
+for(let i = 0; i < arr.length; i++) {
+  for(let j = i+1; j < arr.length; j++) {
+
+    if(arr[i] > arr[j]) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+
+  }
+}
+
+// descending
+// for(let i = 0; i < arr.length; i++) {
+//   for(let j = i+1; j < arr.length; j++) {
+
+//     if(arr[i] < arr[j]) {
+//       let temp = arr[j];
+//       arr[j] = arr[i];
+//       arr[i] = temp;
+//     }
+
+//   }
+// }
+
+console.log(arr);
